d80d1c15c5bc708cab8fafddddf56cfd3826673b
Mohsin <ahme.mohsin@gmail.com>
2024-07-24 01:03:38 +0500

    modified QuoteCard.jsx

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -12,23 +12,6 @@ const QuoteCard = () => {
     text: '',
     author: '',
   });
-  const quotes = JSON.parse(localStorage.getItem('quotes'));
-  useEffect(() => {
-    if (!quotes) {
-      try {
-        const fetchQuotes = async () => {
-          const response = await axios.get(REACT_APP_API_URL);
-          localStorage.setItem('quotes', JSON.stringify(response.data));
-          return response;
-        };
-        debugger;
-        fetchQuotes();
-      } catch {
-        setError('Something Went Wrong.');
-      }
-    }
-  }, [quotes, foundInLocal]);
-
   useEffect(() => {
     const quotes = JSON.parse(localStorage.getItem('quotes'));
     if (!quotes) {
